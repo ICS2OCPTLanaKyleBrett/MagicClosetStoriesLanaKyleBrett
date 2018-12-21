e45454a3c975a41ad2a5cfa673231a720d5272e0
Kylechristensen8842 <43419336+Kylechristensen8842@users.noreply.github.com>
2018-12-21 14:29:10 -0500

    main lua and lvl3

--- main.lua
+++ main.lua
@@ -17,8 +17,4 @@ local composer = require( "composer" )
 -----------------------------------------------------------------------------------------
 
 -- Go to the intro screen
-<<<<<<< HEAD
 composer.gotoScene( "level3_screen" )
-=======
-composer.gotoScene( "splash_screen" )
->>>>>>> 6b7c377b29447fe37a820471fa50f641eef6174f
