d9479e134132f263da186a33ad65377f80faeec0
LanaZahrEddin <43419131+LanaZahrEddin@users.noreply.github.com>
2019-01-11 14:20:53 -0500

    added stuff

--- main.lua
+++ main.lua
@@ -16,6 +16,22 @@ local composer = require( "composer" )
 
 -----------------------------------------------------------------------------------------
 
+--get time in seconds
+local now = os.time()
+
+--and milliseconds since app was opened
+local millis = system.getTimer()
+
+--concatenate to make a longer unique number
+local newSeed = tostring(now)..tostring(millis)
+
+--convert tonumber just to make sure randomseed accepts it
+--this seems to round to an int which is fine as number should still be unique
+newSeed = tonumber(newSeed)
+
+-- code to make sure to get randomization on iPad
+math.randomseed(os.time())
+
 -- Go to the intro screen
 composer.gotoScene( "splash_screen" )
 
